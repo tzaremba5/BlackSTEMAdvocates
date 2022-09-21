140090478aa083594deec26b1e7502466dcb1086
Ted Zaremba <teddyzaremba@Teds-Mac-mini.local>
2022-09-21 00:08:49 -0500

    everything

--- components/Mission.js
+++ components/Mission.js
@@ -2,15 +2,30 @@ import React from 'react'
 
 function Mission() {
   return (
-    <div className=" min-h-[400px]">
+    <div className="min-h-[400px]">
       <div className="ml-5 text-[20px] md:ml-20 font-bold">
         Our Mission
       </div>
-      <div className="bg-white
-      border-4 border-gray-900
-      w-full max-w-[500px] h-[400px]">
+      <div className='flex'>
 
+        <div className="bg-white
+        border-4 border-gray-900
+        w-full max-w-[500px] grow">
+          BE THE CHANGE
+
+          We are excited to host virtual workshops and webinars tailored to the academic, private sector, and corporate sectors. These workshops have been crafted by Dr. Jones, to be interactive, engaging, and thought-provoking. Workshops are also designed for Dr. Jones to travel to your city, and your institution to build community. Please contact us for more details. We will post more detail as we enter the middle part of July 2021. 
+        </div>
+        <div class="flex-col justify-center max-w-4xl max-h-[10px] m-4 p-4 text-gray-800 bg-yellow-300 rounded-lg shadow">
+          <div class="mb-2">
+            <div class="h-3 text-3xl text-left text-gray-600">“</div>
+            <p class="px-4 text-[20px] text-center text-gray-600">
+              "Your heart is worth every beat" Dr. Derick D. Jones JR.
+            </p>
+            <div class="h-3 text-3xl text-right text-gray-600">”</div>
+          </div>
+        </div>
       </div>
+
     </div>
   )
 }
